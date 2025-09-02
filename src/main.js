073739f023a7692843553aee9841ed48a06dd66a
CommieP <229662497+adityade8k@users.noreply.github.com>
2025-09-02 12:21:35 -0400

    CSS Setup
    Setup css.
    learned about variables.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // alias import test
 import { ping } from "./utils/ping";
+//styles import
+import './styles/base.css'
 
 // basic setup
 let app = document.getElementById("app");
+
+
 
 const h1 = document.createElement("h1");
 h1.textContent = "This works?";
