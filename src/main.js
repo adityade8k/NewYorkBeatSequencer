2b6ad228919c1c96c456749f6473eda0878a3df3
CommieP <229662497+adityade8k@users.noreply.github.com>
2025-09-02 09:14:35 -0400

    Src, index.html and main.js created

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// basic setup
+let app = document.getElementById("app");
+
+const h1 = document.createElement("h1");
+h1.textContent = "This works?";
+
+const p1 = document.createElement("p");
+p1.textContent = "hello world";
+
+app.append(h1, p1);
