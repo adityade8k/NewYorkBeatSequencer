6ea433efac74c863be962d1bf9df2825c688b890
CommieP <229662497+adityade8k@users.noreply.github.com>
2025-09-05 16:29:58 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createRouter } from '@lib/router/router.js'
 import { Nav } from '@components/Nav/Nav.js'
 import { Home } from '@pages/home.js'
 import { About } from '@pages/about.js'
-import { ThreeBackground } from '@components/ThreeBG/ThreeBackground.js' // <-- add this
+import { ThreeBackground } from '@components/ThreeBg/ThreeBackground'
 
 const app = document.getElementById('app')
 
@@ -13,9 +13,9 @@ const three = ThreeBackground()
 document.body.prepend(three.el)
 
 // now mount persistent nav on top
-const nav = Nav()
-document.body.prepend(nav.el)
+const nav = Nav();
+document.body.prepend(nav.el);
 
 // router as before
 const routes = { '/': Home, '/about': About }
-createRouter({ app, routes }).start()
+createRouter({ app, routes }).start();
