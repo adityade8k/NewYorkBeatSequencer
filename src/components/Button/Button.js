76bd2c871ad62bee8c87e41137094bec54b667b8
CommieP <229662497+adityade8k@users.noreply.github.com>
2025-09-06 13:34:25 -0400

    Set up project
    Cleared unused files set up button css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,30 +2,42 @@ import { el } from '@utils/dom.js'
 import './styles.css'
 
 export function Button({
-    label = "Click",
-    onClick,
-    variant = "default",
-    disabled = false,
-    className = '',
-    type = 'buttton',
-    attrs = {}
+  label = 'Click',
+  onClick,
+  variant = 'default',   // supports 'on' | 'off' | others
+  disabled = false,
+  className = '',
+  type = 'button',
+  attrs = {}
 } = {}) {
-    const classes = ['btn'];
-    if (variant && variant !== 'default') classes.push(`btn--${variant}`)
-    if (className) {
-        if (Array.isArray(className)) classes.push(...className.filter(Boolean))
-        else classes.push(className)
-    }
+  const classes = ['btn']
+  if (variant && variant !== 'default') classes.push(`btn--${variant}`)
+  if (className) {
+    if (Array.isArray(className)) classes.push(...className.filter(Boolean))
+    else classes.push(className)
+  }
 
-    return el(
-        'button',
-        {
-            class: classes.join(' '),
-            type,
-            disabled: disabled ? '' : null,
-            onClick: onClick ? onClick : null,
-            ...attrs 
-        },
-        label
-    )
+  const computedDisabled =
+    variant === 'on'  ? true  :
+    variant === 'off' || "off-yellow" ? false :
+    !!disabled
+
+  // Optional: if you're treating it like a toggle button, expose aria-pressed
+  const ariaPressed =
+    (variant === 'on' && 'true') ||
+    (variant === 'off' && 'false') ||
+    null
+
+  return el(
+    'button',
+    {
+      class: classes.join(' '),
+      type,
+      disabled: computedDisabled ? '' : null,             
+      onClick: computedDisabled ? null : (onClick || null), // don't wire if disabled
+      'aria-pressed': ariaPressed,                      
+      ...attrs
+    },
+    label
+  )
 }
