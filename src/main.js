a169901a02cbf851c030bb574238b6d0d69a61d0
CommieP <229662497+adityade8k@users.noreply.github.com>
2025-09-06 15:01:48 -0400

    Componet set up and Layout
    Basic grid layout done and components basic mounting done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import { Container } from '@components/Container/Container.js'
+import { FileLoader } from '@components/packages/FileLoader/FileLoader.js'
 import './styles/base.css'
-import { Home } from '@pages/home.js'
+import { el } from '@utils/dom.js'
 
 const app = document.getElementById('app')
 
-const home = Home({mount: app});
+const fileLoader = FileLoader({className: "", attrs: {}}, )
+const fileLoader1 = FileLoader({className: "", attrs: {}}, )
+const fileLoader2 = FileLoader({className: "", attrs: {}}, )
+const topCont = Container({className: "noStroke", attrs: {}}, fileLoader.el, fileLoader1.el)
+const bottomCont = Container({className: "noStroke", attrs: {}}, fileLoader2.el)
+
+app.append(topCont, bottomCont)
